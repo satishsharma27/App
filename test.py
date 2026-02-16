1cba4c5535c3ed12ae92e1fbd519d2f7c103221a
satishsharma27 <satish.sharma2709@gmail.com>
2026-02-16 23:51:14 +0530

    testing

---
+++ test.py
@@ -0,0 +1,9 @@
+# Take input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Add numbers
+sum_result = num1 + num2
+
+# Show result
+print("Sum is:", sum_result)
